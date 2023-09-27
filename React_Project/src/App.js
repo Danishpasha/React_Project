136622832dc5062c0ef4c39a936b8d3f8f6231e0
Danish Pasha <danishpashadp007@gmail.com>
2023-09-27 15:03:48 +0530

    Page Not Found File pushed

--- React_Project/src/App.js
+++ React_Project/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hello from './Hello/Hello'; // Update the import paths
 import Signup from './Signup/Signup';
+import PageNotFound from './PageNotFound/PageNotFound';
 
 
 
@@ -14,6 +15,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Hello />} />
             <Route path="/Signup" element={<Signup />} />
+            <Route path="*" element={<PageNotFound />} />
+
           </Routes>
         </div>
       </Router>
---
+++ React_Project/src/PageNotFound/PageNotFound.js
@@ -0,0 +1,20 @@
+// Hello.js
+import React from 'react';
+
+import './PageNotFound.css';
+
+
+
+
+function PageNotFound() {
+  return (
+    
+    <div classname="Pagenotfound">
+    <h1>Error 404 Page not  Found.</h1>
+   
+    
+  </div>
+  );
+}
+
+export default PageNotFound;
